a8b3e246e39b8681c632fd9bd757bf9223ef5de7
Bradeno <bradeno@gmail.com>
2017-07-05 01:04:12 -0600

    Add files via upload

--- PoEParse/PoEParse/PoEFuncs.cs
+++ PoEParse/PoEParse/PoEFuncs.cs
@@ -23,8 +23,12 @@ namespace PoEParse
                 var Json_Data = w.DownloadString("http://betaapi.pathofexile.com/api/public-stash-tabs?id=" + nextStashId);
                 StashData root = JsonConvert.DeserializeObject<StashData>(Json_Data);
 
-                SaveChangeId(root);
-                SaveStashData(root);
+                //SaveChangeId(root);
+                //SaveStashData(root);
+
+
+                //shove all the data in datatables for processing.
+                ListEverythingTest(root);
 
             }
         }
@@ -96,6 +100,125 @@ namespace PoEParse
                 comm.ExecuteNonQuery();
                 comm.Parameters.Clear();
             }
+
+        }
+
+        //trying to make a do everything loop to populate all list objects.
+        public void ListEverythingTest (StashData root)
+        {
+            ListtoDataTableConverter dataTableConverter = new ListtoDataTableConverter();
+
+            List<StashData> ChangeIdList = new List<StashData>();
+            List<Stash> StashList = new List<Stash>();
+            List<Item> ItemList = new List<Item>();
+            List<Socket> SocketList = new List<Socket>();
+            List<Property1> PropertyList1 = new List<Property1>();
+            List<Additionalproperty> AdditionalPropertyList = new List<Additionalproperty>();
+            List<Requirement> RequirementList = new List<Requirement>();
+            List<Socketeditem> SocketedItemList = new List<Socketeditem>();
+            List<Property2> PropertyList2 = new List<Property2>();
+            List<Additionalproperty1> AdditionalPropertyList2 = new List<Additionalproperty1>();
+            List<Requirement1> RequirementList2 = new List<Requirement1>();
+            List<Nextlevelrequirement> NextLevelRequirementList = new List<Nextlevelrequirement>();
+            List<Nextlevelrequirement1> NextLevelRequirementList2 = new List<Nextlevelrequirement1>();
+
+            //next change ID
+            ChangeIdList.Add(root);
+
+            //start megaloop
+            foreach (Stash stash in root.stashes)
+            {
+                StashList.Add(stash);
+
+                foreach (Item item in stash.items)
+                {
+                    ItemList.Add(item);
+
+                    foreach (Socket socket in item.sockets)
+                    {
+                        SocketList.Add(socket);
+                    }
+
+                    foreach (Socketeditem socketedItem in item.socketedItems)
+                    {
+                        SocketedItemList.Add(socketedItem);
+
+                        foreach (Requirement1 requirement1 in socketedItem.requirements)
+                        {
+                            RequirementList2.Add(requirement1);
+                        }
+
+                        if (socketedItem.nextLevelRequirements != null)
+                        {
+                            foreach (Nextlevelrequirement nextLevelReq in socketedItem.nextLevelRequirements)
+                            {
+                                NextLevelRequirementList.Add(nextLevelReq);
+                            }
+
+                        }
+
+                        foreach (Property2 property2 in socketedItem.properties)
+                        {
+                            PropertyList2.Add(property2);
+                        }
+
+                        foreach (Additionalproperty1 additionalProps1 in socketedItem.additionalProperties)
+                        {
+                            AdditionalPropertyList2.Add(additionalProps1);
+                        }
+                    }
+
+                    if (item.nextLevelRequirements != null)
+                    {
+                        foreach (Nextlevelrequirement1 nextLevelReq1 in item.nextLevelRequirements)
+                        {
+                            //NextLevelRequirementList2.Add(nextLevelReq1);
+                        }
+                    }                    
+
+                    if (item.properties != null)
+                    {
+                        foreach (Property1 property1 in item.properties)
+                        {
+                            PropertyList1.Add(property1);
+                        }
+                    }
+                    
+
+                    if (item.additionalProperties != null)
+                    {
+                        foreach (Additionalproperty additionalProp in item.additionalProperties)
+                        {
+                            AdditionalPropertyList.Add(additionalProp);
+                        }
+                    }                    
+
+                    if (item.requirements != null)
+                    {
+                        foreach (Requirement requirement in item.requirements)
+                        {
+                            RequirementList.Add(requirement);
+                        }
+                    }
+                    
+                }                
+            }
+
+            //Convert each list to a datatable
+            DataTable ChangeId_DT = dataTableConverter.ToDataTable(ChangeIdList);
+            DataTable Stashes_DT = dataTableConverter.ToDataTable(StashList);
+            DataTable Items_DT = dataTableConverter.ToDataTable(ItemList);
+            DataTable SocketedItems_DT = dataTableConverter.ToDataTable(SocketedItemList);
+            DataTable Sockets_DT = dataTableConverter.ToDataTable(SocketList);
+            DataTable Properties1_DT = dataTableConverter.ToDataTable(PropertyList1);
+            DataTable Properties2_DT = dataTableConverter.ToDataTable(PropertyList2);
+            DataTable AdditionalProperties1_DT = dataTableConverter.ToDataTable(AdditionalPropertyList);
+            DataTable AdditionalProperties2_DT = dataTableConverter.ToDataTable(AdditionalPropertyList2);
+            DataTable Requirements1_DT = dataTableConverter.ToDataTable(RequirementList);
+            DataTable Requirements2_DT = dataTableConverter.ToDataTable(RequirementList2);                 
+            DataTable NextLevelRequirement1_DT = dataTableConverter.ToDataTable(NextLevelRequirementList);
+            DataTable NextLevelRequirement2_DT = dataTableConverter.ToDataTable(NextLevelRequirementList2);
+
 
         }
 
